57576ec8d1f50ad01712f690bc167483c9fbaf85
Evan Lightcap <evan@elightcap.com>
2022-04-01 16:00:31 -0400

    no async

--- requestbot.py
+++ requestbot.py
@@ -13,7 +13,7 @@ from dotenv import load_dotenv
 
 
 load_dotenv()
-app = AsyncApp(
+app = App(
     token = os.getenv('SLACK_BOT_TOKEN'),
     signing_secret = os.getenv('SLACK_SIGNING_SECRET')
 )
@@ -36,7 +36,7 @@ MOVIE_COMMAND = "requestmovie"
 TV_COMMAND = "requesttv"
 
 @app.message(MOVIE_COMMAND)
-async def request_movie(message, say):
+def request_movie(message, say):
     messageText = ((message['text']).replace(MOVIE_COMMAND, "")).strip()
     movieUrlEncode = urllib.parse.quote(messageText)
     dbReqURL = movie_url + movieUrlEncode
@@ -66,8 +66,8 @@ async def request_movie(message, say):
                     },
                     "value": id
                 })
-        await say("Please select a movie:")
-        await say(
+        say("Please select a movie:")
+        say(
             blocks=blocks,
             text="Please select the movie you would like to request"
         )
@@ -80,10 +80,10 @@ async def request_movie(message, say):
         ombiJson = json.dumps(ombiBody)
         ombiReq = requests.post(ombi_movie_url, data=ombiJson, headers=ombi_headers)
         ombiMovieLink = ombi_base_url + "/details/movie/" + str(movieID)
-        await say(f"Requesting <{ombiMovieLink}|{movieName}>!")
+        say(f"Requesting <{ombiMovieLink}|{movieName}>!")
 
 @app.message(TV_COMMAND)
-async def request_tv(message, say):
+def request_tv(message, say):
     messageText = ((message['text']).replace(TV_COMMAND, "")).strip()
     tvUrlEncode = urllib.parse.quote(messageText)
     dbReqURL = tv_url + tvUrlEncode
@@ -113,8 +113,8 @@ async def request_tv(message, say):
                     },
                     "value": id
                 })
-        await say("Please Select a Show:")
-        await say(
+        say("Please Select a Show:")
+        say(
             blocks=blocks,
             text="Select a TV show to request!"
         )
@@ -126,65 +126,65 @@ async def request_tv(message, say):
         ombiJson = json.dumps(ombiBody)
         ombiReq = requests.post(ombi_tv_url, data=ombiJson, headers=ombi_headers)
         ombiTVLink = ombi_base_url + "/details/tv/" + str(tvID)
-        await say(f"Requesting <{ombiTVLink}|{tvName}>!")
+        say(f"Requesting <{ombiTVLink}|{tvName}>!")
 
 @app.event("app_mention")
-async def app_mention(event, say):
+def app_mention(event, say):
     messageText = event['text']
     messageText = (messageText.replace("<@U039Y3P5SQH>", "")).strip()
     messageJson = {"text": messageText}
     if TV_COMMAND in messageText:
-        await request_tv(messageJson, say)
+        request_tv(messageJson, say)
     elif MOVIE_COMMAND in messageText:
-        await request_movie(messageJson, say)
+        request_movie(messageJson, say)
     elif "help" in messageText:
         print("help")
-        await help_message(messageText,say)
+        help_message(messageText,say)
     
 
 @app.action(re.compile("^movie_request_button\d+$"))
-async def handle_movie_button(ack, body, say):
+def handle_movie_button(ack, body, say):
     responseURL = body['response_url']
     response = requests.post(responseURL)
-    await ack()
+    ack()
     movieID = body['actions'][0]['value']
     movieName = body['actions'][0]['text']['text']
     ombiBody = {"theMovieDbId": movieID, "languageCode": "EN", "is4kRequest": False}
     ombiJson = json.dumps(ombiBody)
     ombiReq = requests.post(ombi_movie_url, data=ombiJson, headers=ombi_headers)
     ombiMovieLink = ombi_base_url + "/details/movie/" + str(movieID)
-    await say(f"Requesting <{ombiMovieLink}|{movieName}>!")
+    say(f"Requesting <{ombiMovieLink}|{movieName}>!")
 
 @app.action(re.compile("^tv_request_button\d+$"))
-async def handle_tv_button(ack, body, say):
+def handle_tv_button(ack, body, say):
     responseUrl = body['response_url']
     response = requests.post(responseUrl)
-    await ack()
+    ack()
     tvID = body['actions'][0]['value']
     tvName = body['actions'][0]['text']['text']
     ombiBody = {"theMovieDbId": tvID, "requestAll": True, "latestSeason": True, "firstSeason": True}
     ombiJson = json.dumps(ombiBody)
     ombiReq = requests.post(ombi_tv_url, data=ombiJson, headers=ombi_headers)
     ombiMovieLink = ombi_base_url + "/details/tv/" + str(tvID)
-    await say(f"Requesting <{ombiMovieLink}|{tvName}>!")
+    say(f"Requesting <{ombiMovieLink}|{tvName}>!")
 
 @app.command("/requestmovie")
-async def handle_movie_command(ack, body, logger, say):
-    await ack()
+def handle_movie_command(ack, body, logger, say):
+    ack()
     text = body['text']
     messageJson = {"text": text}
-    await request_movie(messageJson, say)
+    request_movie(messageJson, say)
 
 @app.command("/requesttv")
-async def handle_tv_command(ack, body, logger, say):
-    await ack()
+def handle_tv_command(ack, body, logger, say):
+    ack()
     text = body['text']
     messageJson = {"text": text}
-    await request_tv(messageJson, say)
+    request_tv(messageJson, say)
 
 @app.command("/requestinvite")
-async def handle_invite_command(ack, body, logger, say):
-    await ack()
+def handle_invite_command(ack, body, logger, say):
+    ack()
     text = body['text']
     inviterequest = f"<@{body['user_id']}> has requested an invite! Email: {text}"
     mBlocks = [{
@@ -213,20 +213,20 @@ async def handle_invite_command(ack, body, logger, say):
             }
         ]
     }]
-    await app.client.chat_postMessage(
+    app.client.chat_postMessage(
         channel="UERHVSNFL",
         text=inviterequest
     )
 
-    await app.client.chat_postMessage(
+    app.client.chat_postMessage(
         channel="UERHVSNFL",
         text=inviterequest,
         blocks=mBlocks
     )
 
 @app.action("invite_approve_button")
-async def handle_invite_approve_button(ack, body, say):
-    await ack()
+def handle_invite_approve_button(ack, body, say):
+    ack()
     email = body['actions'][0]['value']
     msg= body['message']['text']
     split = msg.split(" ")
@@ -238,11 +238,11 @@ async def handle_invite_approve_button(ack, body, say):
     jfinJson = json.dumps(jfinBody)
     jfin_url = jellyfin_url + "/Users/New"
     jfinReq = requests.post(jfin_url, data=jfinJson, headers=jellyfin_headers)
-    await say("Approved!")
+    say("Approved!")
 
 
 @app.message(re.compile("(^help$)"))
-async def help_message(message, say):
+def help_message(message, say):
     msg = "Hi im RequestBot, a bot to help you download movies and tv shows! Please choose an option below!"
 
     blocks = [
@@ -272,37 +272,37 @@ async def help_message(message, say):
                 }
             ]
         }]
-    await say(f"{msg}")
-    await say(
+    say(f"{msg}")
+    say(
         blocks=blocks, text="Select an option!"
         )
 
 @app.action("tv_help_button")
-async def tv_help_button(ack, body, say):
-    await ack()
-    await help_tv(body, say)
+def tv_help_button(ack, body, say):
+    ack()
+    help_tv(body, say)
 
 @app.action("movie_help_button")
-async def movie_help_button(ack, body, say):
-    await ack()
-    await help_movie(body, say)
+def movie_help_button(ack, body, say):
+    ack()
+    help_movie(body, say)
 
 @app.message("help movie")
-async def help_movie(message, say):
+def help_movie(message, say):
     msg1 = "To request a movie, just type `requestmovie` followed by the name of the movie you want to request! For example, `requestmovie The Matrix`. You can also just mention me in any channel and I'll try to find the movie for you, for example `@RequestBot The Matrix`"
     msg2 = "Slash commands are also supported, for example `/requestmovie The Matrix`"
-    await say(f"{msg1}")
-    await say(f"{msg2}")
+    say(f"{msg1}")
+    say(f"{msg2}")
 
 @app.message("help tv")
-async def help_tv(message, say):
+def help_tv(message, say):
     msg1 = "To request a tv show, just type `requesttv` followed by the name of the tv show you want to request! For example, `requesttv The Simpsons`. You can also just mention me in any channel and I'll try to find the tv show for you, for example `@RequestBot The Simpsons`"
     msg2 = "Slash commands are also supported, for example `/requesttv The Simpsons`"
-    await say(f"{msg1}")
-    await say(f"{msg2}")
+    say(f"{msg1}")
+    say(f"{msg2}")
 
 @app.event("message")
-async def handle_message_events(body, logger):
+def handle_message_events(body, logger):
     logger.info(body)
 
 if __name__ == "__main__":
