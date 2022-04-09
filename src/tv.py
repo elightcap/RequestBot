ca48a638271b654f3326a2d51e2eb3dda92d9b4b
Evan Lightcap <elightcap@elightcap.com>
2022-04-09 14:30:27 -0400

    handle tv key error

--- src/tv.py
+++ src/tv.py
@@ -16,16 +16,18 @@ MOVIE_DB_HEADERS = {'Authorization': f'Bearer {MOVIE_DB_API_KEY}'}
 TV_URL = "https://api.themoviedb.org/4/search/tv?query="
 
 def tv_req(ack,body):
-    text = body['text']
-    if text == "":
+    ack()
+    try:
+        text = body['text']
+        tvUrlEncode = urllib.parse.quote(text)
+        dbReqUrl = TV_URL + tvUrlEncode
+        dbReq = requests.get(dbReqUrl, headers=MOVIE_DB_HEADERS)
+        dbReqJson = json.loads(dbReq.text)
+        print(dbReqJson)
+    except KeyError as e:
         app.client.chat_postMessage(
             channel=body['user_id'],
             text="Please enter a tv show name with your request"
         )
         return
-    tvUrlEncode = urllib.parse.quote(text)
-    dbReqUrl = TV_URL + tvUrlEncode
-    dbReq = requests.get(dbReqUrl, headers=MOVIE_DB_HEADERS)
-    dbReqJson = json.loads(dbReq.text)
-    print(dbReqJson)
 
