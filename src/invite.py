f7ccd91a260a27cf0f141d0bb624de2f52f8c7e5
Evan Lightcap <elightcap@elightcap.com>
2022-04-09 17:09:47 -0400

    linting

--- src/invite.py
+++ src/invite.py
@@ -1,9 +1,9 @@
-import requests
 import os
-import requests
 import random
 import string
 import json
+from urllib.error import HTTPError
+import requests
 
 from dotenv import load_dotenv
 from slack_bolt import App
@@ -19,14 +19,20 @@ app = App(
 ADMIN_ID = os.getenv('SLACK_ADMIN_ID')
 JELLYFIN_API_KEY = os.getenv('JELLYFIN_API_KEY')
 JELLYFIN_URL = os.getenv('JELLYFIN_URL')
-JELLYFIN_HEADERS = {"X-Emby-Authorization": "Mediabrowser Token=" + JELLYFIN_API_KEY, "Content-Type": "application/json"}
+JELLYFIN_HEADERS = {
+    "X-Emby-Authorization": "Mediabrowser Token=" + JELLYFIN_API_KEY,
+    "Content-Type": "application/json"
+    }
+PW_NUMBER = 12
+
 
 def invite_req(ack,body):
+    """function handles invite request.  Sends button to admin for approval"""
     ack()
     try:
         text = body['text']
-        inviteMsg = f"<@{body['user_id']}> has requested an invite! Email: {text}"
-        mBlocks = [{
+        invite_msg = f"<@{body['user_id']}> has requested an invite! Email: {text}"
+        m_blocks = [{
         "type": "actions",
         "block_id": "invite_request_actions",
         "elements": [
@@ -54,16 +60,16 @@ def invite_req(ack,body):
     }]
         app.client.chat_postMessage(
             channel=ADMIN_ID,
-            text=inviteMsg
+            text=invite_msg
     )
 
         app.client.chat_postMessage(
             channel=ADMIN_ID,
-            text=inviteMsg,
-            blocks=mBlocks
+            text=invite_msg,
+            blocks=m_blocks
         )
-    except KeyError as e:
-        print(e)
+    except KeyError as error:
+        print(error)
         app.client.chat_postMessage(
             channel=body['user_id'],
             text="Please enter your email address to invite!"
@@ -72,29 +78,36 @@ def invite_req(ack,body):
 
 
 def invite_approve_actions(ack,body):
+    """function handles invite approval.  If approved, creates user in jellyfin and sends invite"""
     ack()
     email = body['actions'][0]['value']
     msg = body['message']['text']
     split = msg.split(" ")
     user = split[0]
     user = user.replace("<@", "").replace(">", "")
-    N=12
-    pw = ''.join(random.choices(string.ascii_uppercase + string.digits, k=N))
-    jfinBody = {"Name": email, "Password": pw}
-    jfinJson = json.dumps(jfinBody)
+    p_w = ''.join(random.choices(string.ascii_uppercase + string.digits, k=PW_NUMBER))
+    jfin_body = {"Name": email, "Password": p_w}
+    jfin_json = json.dumps(jfin_body)
     jfin_url = JELLYFIN_URL + "/Users/New"
-    jfinReq = requests.post(jfin_url, data=jfinJson, headers=JELLYFIN_HEADERS)
+    try:
+        requests.post(jfin_url, data=jfin_json, headers=JELLYFIN_HEADERS)
+    except HTTPError as err:
+        print(err)
+        return
     app.client.chat_postMessage(
         channel=ADMIN_ID,
         text="Approved!"
     )
     app.client.chat_postMessage(
         channel=user,
-        text=f"Your request has been approved! please login to {JELLYFIN_URL} with the following credentials: \n Username: {email} \n Password: {pw} \n Please change your password after logging in."
+        text=f"""Your request has been approved!
+         please login to {JELLYFIN_URL} with the following credentials: 
+        \n Username: {email} \n Password: {p_w} \n Please change your password after logging in."""
     )
 
 
 def invite_deny_actions(ack,body):
+    """function handles invite denial.  Sends message to user that invite was denied"""
     ack()
     msg= body['message']['text']
     split = msg.split(" ")
@@ -106,5 +119,7 @@ def invite_deny_actions(ack,body):
     )
     app.client.chat_postMessage(
         channel=user,
-        text="Sorry, your request has been denied.  Please message the admin if you have any questions."
+        text="""Sorry, your request has been denied.
+         Please message the admin if you have any questions."""
     )
+    
--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-import os
+import os 
 import re
 
 from helper import helperfunc,helper_button_actions
@@ -15,48 +15,57 @@ app = App(
     token = os.getenv('SLACK_BOT_TOKEN'),
     signing_secret = os.getenv('SLACK_SIGNING_SECRET')
 )
-#CHAT = app.client.chat_postMessage()
 
 @app.command("/help")
-def help(ack,body,logger):
+def req_help(ack,body):
+    """listener for help function"""
     helperfunc(ack,body)
 
 @app.action(re.compile("helper_button$"))
-def handle_helper_button(ack, body, logger):
+def handle_helper_button(ack, body):
+    """listener for helper button"""
     helper_button_actions(ack,body)
 
 
 @app.command("/requesttv")
-def tvreq(ack,body,logger):
+def tvreq(ack,body):
+    """listener for tv request function"""
     tv_req(ack,body)
 
-@app.action(re.compile("^tv_request_button\d+$"))
-def handle_tv_request_button(ack, body, logger):
+@app.action(re.compile(r"^tv_request_button\d+$"))
+def handle_tv_request_button(ack, body):
+    """listener for tv request button"""
     tv_button_actions(ack,body)
 
 @app.command("/requestmovie")
-def moviereq(ack,body,logger):
+def moviereq(ack,body):
+    """listener for movie request function"""
     movie_req(ack,body)
 
-@app.action(re.compile("^movie_request_button\d+$"))
-def handle_movie_request_button(ack, body, logger):
+@app.action(re.compile(r"^movie_request_button\d+$"))
+def handle_movie_request_button(ack, body):
+    """listener for movie request button"""
     movie_button_actions(ack,body)
 
 @app.command("/requestinvite")
-def invitereq(ack,body,logger):
+def invitereq(ack,body):
+    """listener for invite request function"""
     invite_req(ack,body)
 
 @app.action("invite_approve_button")
-def handle_invite_approve_button(ack, body, logger):
+def handle_invite_approve_button(ack, body):
+    """listener for invite approve button"""
     invite_approve_actions(ack,body)
 
 @app.action("invite_deny_button")
-def handle_invite_deny_button(ack, body, logger):
+def handle_invite_deny_button(ack, body):
+    """listener for invite deny button"""
     invite_deny_actions(ack,body)
 
 
 @app.event("message")
-def handle_message_events(body, logger):
+def handle_message_events():
+    """listener for message events"""
     return
 
 if __name__ == "__main__":
