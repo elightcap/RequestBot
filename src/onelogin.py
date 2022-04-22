cfcb06d5f093b35be93c6da2ef8cd4ad85d971eb
Evan Lightcap <elightcap@elightcap.com>
2022-04-21 22:01:18 -0400

    ack(

--- src/onelogin.py
+++ src/onelogin.py
@@ -1,6 +1,5 @@
 def onelogin_response(message,say):
     """respond to onelogin"""
-    ack()
     author = message['user']
     msg = f"""Hi <@{author}>! I see you are asking about OneLogin.
      I'm excited to inform you that we have moved to Okta as an Identity Provider. 
