dacc1741a61230438fd339bc57fb690b35d6f64a
Evan Lightcap <evan@elightcap.com>
2022-12-05 22:46:27 -0500

    bad example

--- app.py
+++ app.py
@@ -7,8 +7,14 @@ from urllib.error import HTTPError
 from flask import Flask,jsonify,request
 
 OMBI_API_KEY = "something here"
-
 OMBI_HEADERS = {"ApiKey": OMBI_API_KEY, "Content-Type": "application/json"}
+
+JELLYFIN_API_KEY = os.getenv('JELLYFIN_API_KEY')
+JELLYFIN_URL = os.getenv('JELLYFIN_URL')
+JELLYFIN_HEADERS = {
+    "X-Emby-Authorization": "Mediabrowser Token=" + JELLYFIN_API_KEY,
+    "Content-Type": "application/json"
+    }
 
 app = Flask(__name__)
 
@@ -17,24 +23,15 @@ app = Flask(__name__)
 def hello_world():
     return "Hello, World!"
 
-@app.route('/post', methods=["POST"])
-def testpost():
-    input_json = request.get_json(force=True)
-    print(input_json)
-    dictToReturn = {'text':input_json['text']}
-    print(dictToReturn)
-    return jsonify(dictToReturn)
 
 @app.route('/notify', methods=["POST"])
 def notify_slack():
     input_json = request.get_json(force=True)
     print(input_json)
-    dictToReturn = {'text':input_json['text']}
-    print(dictToReturn)
     requestId = input_json['requestId']
     getUrl = "https://ombi.elightcap.com/api/v1/Request/movie/info/{}".format(requestId)
     getRequest = requests.get(getUrl,headers=OMBI_HEADERS)
     alias = getRequest.json()["requestedByAlias"]
     name = getRequest.json()["requestedByAlias"]
     send_slack_notification(alias,name)
-    return jsonify(dictToReturn)
+    return 200
