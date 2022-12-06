fe3cf4c9213314bb640b1d0d1ea0da0a7aa1dba4
Evan Lightcap <evan@elightcap.com>
2022-12-05 21:54:13 -0500

    flask

---
+++ app.py
@@ -0,0 +1,13 @@
+import os
+import re
+
+from urllib.error import HTTPError
+from flask import Flask,jsonify,request
+import requests
+
+app = Flask(__name__)
+
+
+@app.route('/notify')
+def hello_world():
+    return 'This is my first API call!'
